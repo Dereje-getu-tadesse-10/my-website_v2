88b1594e2da3cd6de1e8a1fe4dfcb945688339f1
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-08-30 19:56:20 +0200

    update config cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,31 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import remarkToc from 'remark-toc';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import remarkToc from 'remark-toc'
+import tailwind from '@astrojs/tailwind'
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme: 'dracula'
-    },
-    remarkPlugins: [remarkToc],
-    remarkRehype: {
-      footnoteLabel: 'Footnotes'
-    },
-    gfm: false
-  }), tailwind()],
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        theme: 'dracula',
+      },
+      remarkPlugins: [remarkToc],
+      remarkRehype: {
+        footnoteLabel: 'Footnotes',
+      },
+      gfm: false,
+    }),
+    tailwind(),
+  ],
   experimental: {
-    viewTransitions: true
+    viewTransitions: true,
   },
-  output: "server",
-  adapter: cloudflare()
-});
+  output: 'server',
+  adapter: cloudflare({
+    mode: 'directory',
+  }),
+})
