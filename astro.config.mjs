cab4a6863e928c6241fd5a9aad04e8357c6520a9
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-08-30 20:39:11 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import remarkToc from 'remark-toc';
 import tailwind from '@astrojs/tailwind';
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({
