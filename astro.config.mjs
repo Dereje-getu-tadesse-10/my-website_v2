63579c319d09be6311dbc8de5a909f7463b0889f
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-08-30 20:18:45 +0200

    update; cloudlare config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import remarkToc from 'remark-toc'
 import tailwind from '@astrojs/tailwind'
-
 import cloudflare from '@astrojs/cloudflare'
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
