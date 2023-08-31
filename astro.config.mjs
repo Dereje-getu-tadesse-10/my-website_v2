00a2bda6d149ddc0e1aa73505c03959fdf4b718d
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-08-31 18:12:17 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import remarkToc from 'remark-toc';
-import tailwind from '@astrojs/tailwind';
-
-import deno from "@astrojs/deno";
-
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import remarkToc from 'remark-toc'
+import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme: 'dracula'
-    },
-    remarkPlugins: [remarkToc],
-    remarkRehype: {
-      footnoteLabel: 'Footnotes'
-    },
-    gfm: false
-  }), tailwind()],
-  experimental: {
-    viewTransitions: true
-  },
-  output: "server",
-  adapter: deno()
-});
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        theme: 'dracula',
+      },
+      remarkPlugins: [remarkToc],
+      remarkRehype: {
+        footnoteLabel: 'Footnotes',
+      },
+      gfm: false,
+    }),
+    tailwind(),
+  ],
+})
