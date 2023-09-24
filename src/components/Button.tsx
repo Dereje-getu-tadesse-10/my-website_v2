796d3be096856656a0f70d986c13b3c62fcb2b86
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-09-24 20:58:46 +0200

    update: button, layout, about section and header

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -29,10 +29,10 @@ const Button = () => {
     }, [index, words]);
 
     return (
-        <a className="bg-rose-500 text-white px-4 py-2 rounded-md font-medium no-underline flex items-center gap-2 text-base w-min" href="mailto:hello@dereje.fr">
-            <div className={`relative w-[130px] overflow-hidden h-6 flex items-center ${isTransitioning ? 'opacity-0 transition-opacity ease-in-out' : ''}`}>
+        <a className="bg-rose-600 text-white px-4 py-2 rounded-md font-medium no-underline flex items-center gap-2 text-base w-full sm:w-min" href="mailto:hello@dereje.fr">
+            <div className={`relative w-full sm:w-[130px] overflow-hidden h-6 flex items-center ${isTransitioning ? 'opacity-0 transition-opacity ease-in-out' : ''}`}>
                 <span className={`absolute w-max transition-all duration-500 ease-in-out ${isTransitioning ? '-translate-y-full' : 'translate-y-0 opacity-100 transition-opacity'}`}>
-            {currentText}
+                {currentText}
             </span>
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-send mt-1">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-
-    },
-  },
+  theme: {},
   plugins: [require('@tailwindcss/typography')],
 }
