3d2bcd45de2b894f82b0042585207a3f4e72c846
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-09-24 21:39:58 +0200

    update: add sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,19 @@ import mdx from '@astrojs/mdx';
 import remarkToc from 'remark-toc';
 import tailwind from '@astrojs/tailwind';
 import partytown from "@astrojs/partytown";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dereje.fr',
   integrations: [mdx({
     syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: 'dracula'
     },
     remarkPlugins: [remarkToc],
-    gfm: false
-  }), tailwind(), partytown(), react()]
+    gfm: false,
+  }), tailwind(), partytown(), react(), sitemap()]
 });
